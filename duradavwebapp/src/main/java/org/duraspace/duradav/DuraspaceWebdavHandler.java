c815e666f6cb33a313d5c081cd8d22a1f7b91034
Chris Wilper <cwilper@gmail.com>
2009-03-04 17:51:29 +0000

    servlet delegates to webdavhandler impl
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@31 3c0c4179-e91d-42e7-a082-8e2792ce3676

---
+++ duradavwebapp/src/main/java/org/duraspace/duradav/DuraspaceWebdavHandler.java
@@ -0,0 +1,49 @@
+package org.duraspace.duradav;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DuraspaceWebdavHandler
+        implements WebdavHandler {
+
+    public void handleGet(HttpServletRequest req, HttpServletResponse resp)
+            throws WebdavException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void handleHead(HttpServletRequest req, HttpServletResponse resp)
+            throws WebdavException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void handleMkCol(HttpServletRequest req, HttpServletResponse resp)
+            throws WebdavException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void handleMove(HttpServletRequest req, HttpServletResponse resp)
+            throws WebdavException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void handleOptions(HttpServletRequest req, HttpServletResponse resp)
+            throws WebdavException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void handlePropFind(HttpServletRequest req, HttpServletResponse resp)
+            throws WebdavException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void handlePropPatch(HttpServletRequest req, HttpServletResponse resp)
+            throws WebdavException {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public void handlePut(HttpServletRequest req, HttpServletResponse resp)
+            throws WebdavException {
+        throw new RuntimeException("Not implemented");
+    }
+
+}
