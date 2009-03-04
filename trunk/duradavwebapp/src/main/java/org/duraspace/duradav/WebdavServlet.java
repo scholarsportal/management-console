965d3e7547224ec165ee310143ffdaffbd24e87e
Chris Wilper <cwilper@gmail.com>
2009-03-04 17:54:21 +0000

    renamed DAVServlet to WebdavServlet
    git-svn-id: https://svn.duraspace.org/duracloud-priv@32 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- trunk/duradavwebapp/src/main/java/org/duraspace/duradav/WebdavServlet.java
+++ trunk/duradavwebapp/src/main/java/org/duraspace/duradav/WebdavServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class DAVServlet extends HttpServlet {
+public class WebdavServlet extends HttpServlet {
 
     public static final long serialVersionUID = 1L;
 
