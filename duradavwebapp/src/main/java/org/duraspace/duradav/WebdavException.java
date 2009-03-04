0ad32d4c73497f979fcc6bf5fc33a57d927e5ea2
Chris Wilper <cwilper@gmail.com>
2009-03-04 19:19:34 +0000

    using slf4j and http status codes for webdavexception
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@33 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- duradavwebapp/src/main/java/org/duraspace/duradav/WebdavException.java
+++ duradavwebapp/src/main/java/org/duraspace/duradav/WebdavException.java
@@ -1,16 +1,44 @@
 package org.duraspace.duradav;
 
+/**
+ * Signals a webdav error.
+ */
 public class WebdavException
         extends Exception {
 
     private static final long serialVersionUID = 1L;
 
-    public WebdavException(String message) {
+    private final int statusCode;
+
+    /**
+     * Creates an instance with a message and http status code.
+     *
+     * @param message the message
+     * @param statusCode the status code
+     */
+    public WebdavException(String message, int statusCode) {
         super(message);
+        this.statusCode = statusCode;
     }
 
-    public WebdavException(String message, Throwable cause) {
+    /**
+     * Creates an instance with a message, http status code, and cause.
+     *
+     * @param message the message
+     * @param statusCode the status code
+     * @param cause the underlying cause of this exception
+     */
+    public WebdavException(String message, int statusCode, Throwable cause) {
         super(message, cause);
+        this.statusCode = statusCode;
     }
 
+    /**
+     * Gets the http status code for this exception.
+     *
+     * @return the status code
+     */
+    public int getStatusCode() {
+        return statusCode;
+    }
 }
