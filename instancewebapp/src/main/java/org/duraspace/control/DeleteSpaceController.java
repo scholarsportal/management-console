d2f8b2545e5af5c1c09b1b58ac8624850e943e7c
Bill Branan <bbranan@duraspace.org>
2009-03-17 21:59:15 +0000

    Adds UI options to delete spaces and toggle space access
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@43 3c0c4179-e91d-42e7-a082-8e2792ce3676

---
+++ instancewebapp/src/main/java/org/duraspace/control/DeleteSpaceController.java
@@ -0,0 +1,54 @@
+package org.duraspace.control;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import org.duraspace.domain.Space;
+import org.duraspace.storage.StorageProvider;
+import org.duraspace.util.SpaceUtil;
+import org.duraspace.util.StorageProviderUtil;
+import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+
+public class DeleteSpaceController extends SimpleFormController {
+
+    protected final Logger log = Logger.getLogger(getClass());
+
+	public DeleteSpaceController()	{
+		setCommandClass(Space.class);
+		setCommandName("space");
+	}
+
+    @Override
+    protected ModelAndView onSubmit(Object command,
+                                    BindException errors)
+    throws Exception {
+        Space space = (Space) command;
+        String accountId = space.getAccountId();
+        String spaceId = space.getSpaceId();
+
+        if(accountId == null || accountId.equals("")) {
+            throw new Exception("Account ID must be provided in order to delete a space.");
+        }
+
+        if(spaceId == null || spaceId.equals("")) {
+            throw new Exception("Space ID must be provided in order to delete a space.");
+        }
+
+        StorageProvider storage =
+            StorageProviderUtil.getStorageProvider(accountId);
+
+        storage.deleteSpace(spaceId);
+
+        List<Space> spaces = SpaceUtil.getSpacesList(accountId);
+
+        ModelAndView mav = new ModelAndView(getSuccessView());
+        mav.addObject("spaces", spaces);
+        mav.addObject("accountId", accountId);
+
+        return mav;
+    }
+
+}
