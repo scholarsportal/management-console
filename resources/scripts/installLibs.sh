2407ae47f5dd5b0da4dd60adf2295e9ef536f6e8
Andrew Woods <awoods@duraspace.org>
2009-04-09 19:01:01 +0000

    This update implements an EC2 Compute Provider using the Typica AWS library. Typica also supports Amazon services: DevPay and FPS. This update is the first in migrating over to the Typica library.
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@72 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- resources/scripts/installLibs.sh
+++ resources/scripts/installLibs.sh
@@ -4,5 +4,7 @@
 
 mvn install:install-file -Dfile=resources/libs/amazon-ec2-2008-12-01-java-library.jar -DgroupId=com.amazonaws -DartifactId=ec2 -Dversion=2008-12-01 -Dpackaging=jar
 mvn install:install-file -Dfile=resources/libs/amazon-ec2-2008-12-01-java-library-src.jar -DgroupId=com.amazonaws -DartifactId=ec2-src -Dversion=2008-12-01 -Dpackaging=jar
+mvn install:install-file -Dfile=resources/libs/aws-typica-1.5.1.jar -DgroupId=com.google.code.typica -DartifactId=typica -Dversion=1.5.1 -Dpackaging=jar
+# mvn install:install-file -Dfile=resources/libs/amazon-ec2-2008-12-01-java-library-src.jar -DgroupId=com.amazonaws -DartifactId=ec2-src -Dversion=2008-12-01 -Dpackaging=jar
 mvn install:install-file -Dfile=resources/libs/rackspace-java-cloudfiles-1.3.0.jar -DgroupId=com.mosso -DartifactId=cloudfiles -Dversion=1.3.0 -Dpackaging=jar
 mvn install:install-file -Dfile=resources/libs/rackspace-java-cloudfiles-1.3.0-src.jar -DgroupId=com.mosso -DartifactId=cloudfiles-src -Dversion=1.3.0 -Dpackaging=jar
