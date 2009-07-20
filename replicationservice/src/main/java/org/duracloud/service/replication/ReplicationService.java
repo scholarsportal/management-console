b16be5558f880991203a3b68f8fbaffae0c0878c
Bill Branan <bbranan@duraspace.org>
2009-07-20 22:16:18 +0000

    Starting point for the replication service, this is really just a shell application for now, but it does build and can be deployed as an OSGi bundle
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@111 3c0c4179-e91d-42e7-a082-8e2792ce3676

---
+++ replicationservice/src/main/java/org/duracloud/service/replication/ReplicationService.java
@@ -0,0 +1,17 @@
+package org.duracloud.service.replication;
+
+public class ReplicationService {
+
+	public void start() throws Exception {
+		System.out.println("Starting Replication Service...");
+
+		System.out.println("Replication Service Started");
+	}
+
+	public void stop() throws Exception {
+	    System.out.println("Stopping Replication Service...");
+
+		System.out.println("Replication Service Stopped");
+	}
+
+}
