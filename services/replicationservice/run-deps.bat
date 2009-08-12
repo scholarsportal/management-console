a8ab64a158c0b7be6dd8769173ae600cb63fc245
Bill Branan <bbranan@duraspace.org>
2009-08-12 22:30:36 +0000

    Allows for running each of the OSGi services independently. From the directory of any of the services (e.g. services/helloservice) use:
    run-deps.(sh|bat)
    mvn clean install -f pom-run.xml pax:provision
    
    The run-deps script creates pom-run.xml by importing the OSGi bundle dependencies necessary to run the service. 
    The run-deps script depends on pax-construct.
    
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@128 3c0c4179-e91d-42e7-a082-8e2792ce3676

---
+++ services/replicationservice/run-deps.bat
@@ -0,0 +1,20 @@
+copy pom.xml pom.xml.bak
+
+REM - Spring DM -
+call pax-import-bundle -g org.springframework.osgi -a spring-osgi-core -v 1.2.0
+call pax-import-bundle -g org.springframework.osgi -a spring-osgi-extender -v 1.2.0
+call pax-import-bundle -g org.springframework.osgi -a spring-osgi-io -v 1.2.0
+
+REM - DuraCloud -
+call pax-import-bundle -g org.duraspace -a storageprovider -v 1.0.0 -- -DimportTransitive -DwidenScope
+call pax-import-bundle -g org.duraspace -a javaclient -v 1.0.0 -- -DimportTransitive -DwidenScope
+
+REM - Other -
+call pax-import-bundle -g org.apache.activemq -a com.springsource.org.apache.activemq -v 5.2.0 -- -DimportTransitive -DwidenScope
+call pax-import-bundle -g javax.activation -a com.springsource.javax.activation -v 1.1.1
+call pax-import-bundle -g org.slf4j -a com.springsource.slf4j.log4j -v 1.5.0 -- -DimportTransitive -DwidenScope
+call pax-import-bundle -g com.thoughtworks.xstream -a com.springsource.com.thoughtworks.xstream -v 1.3.0 -- -DimportTransitive -DwidenScope
+call pax-import-bundle -g org.jdom -a com.springsource.org.jdom -v 1.0.0
+
+copy pom.xml pom-run.xml
+move pom.xml.bak pom.xml
---
+++ services/servicesutil/run-deps.bat
@@ -0,0 +1,18 @@
+copy pom.xml pom.xml.bak
+
+REM - Spring DM -
+call pax-import-bundle -g org.springframework.osgi -a spring-osgi-core -v 1.2.0
+call pax-import-bundle -g org.springframework.osgi -a spring-osgi-extender -v 1.2.0
+call pax-import-bundle -g org.springframework.osgi -a spring-osgi-io -v 1.2.0
+
+REM - DuraCloud -
+call pax-import-bundle -g org.duraspace -a common -v 1.0.0 -- -DimportTransitive -DwidenScope
+
+REM - Other -
+call pax-import-bundle -g org.slf4j -a com.springsource.slf4j.log4j -v 1.5.0 -- -DimportTransitive -DwidenScope
+call pax-import-bundle -g com.thoughtworks.xstream -a com.springsource.com.thoughtworks.xstream -v 1.3.0 -- -DimportTransitive -DwidenScope
+call pax-import-bundle -g org.apache.commons -a com.springsource.org.apache.commons.fileupload -v 1.2.0 -- -DimportTransitive -DwidenScope
+
+copy pom.xml pom-run.xml
+move pom.xml.bak pom.xml
+
