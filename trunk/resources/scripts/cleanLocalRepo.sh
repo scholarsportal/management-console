5223013f02544368c87e1aca905693913ba532cf
Andrew Woods <awoods@duraspace.org>
2009-08-20 04:03:47 +0000

    This update adds a sed replacement of the BUILD_HOME enviroment variable for the nightly DURACLOUD-NIT bamboo plan.
    git-svn-id: https://svn.duraspace.org/duracloud-priv@139 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- trunk/resources/scripts/cleanLocalRepo.sh
+++ trunk/resources/scripts/cleanLocalRepo.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 scriptdir=`dirname "$0"`
+
+echo "Updating env.sh for DURACLOUD-NIT"
+sed_env_cmd="s/DURACLOUD-SAN/DURACLOUD-NIT/"
+sed -i.backup -e $sed_env_cmd $scriptdir/env.sh
+
+
 . "$scriptdir"/common.sh
 
 
