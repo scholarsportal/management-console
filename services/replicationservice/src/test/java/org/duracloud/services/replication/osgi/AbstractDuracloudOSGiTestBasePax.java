c21fcb5e260a41614d8187e462eb84b580dd4ca5
Andrew Woods <awoods@duraspace.org>
2009-08-24 23:01:23 +0000

    This update adds OSGi integration testing to 'replicationservice'. It uses PAX-Exam without the need to hard-code the test dependencies into the test class(es). The update also adds dynamic configuration to the ReplicationService.
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@143 3c0c4179-e91d-42e7-a082-8e2792ce3676

---
+++ services/replicationservice/src/test/java/org/duracloud/services/replication/osgi/AbstractDuracloudOSGiTestBasePax.java
@@ -0,0 +1,44 @@
+
+package org.duracloud.services.replication.osgi;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.BundleContext;
+
+import static org.ops4j.pax.exam.CoreOptions.bundle;
+import static org.ops4j.pax.exam.CoreOptions.equinox;
+import static org.ops4j.pax.exam.CoreOptions.mavenConfiguration;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.provision;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.profile;
+
+@RunWith(JUnit4TestRunner.class)
+public class AbstractDuracloudOSGiTestBasePax {
+
+    @Inject
+    protected BundleContext bundleContext;
+
+    @Before
+    public void setUp() throws Exception {
+        Thread.sleep(2000);
+    }
+
+    @Configuration
+    public static Option[] configuration() {
+
+        Option bundles =
+                provision(bundle("file:target/replicationservice-1.0.0.jar"));
+
+        return options(bundles,
+                       mavenConfiguration(),
+                       equinox(),
+                       profile("spring.dm"),
+                       profile("log"));
+    }
+
+}
