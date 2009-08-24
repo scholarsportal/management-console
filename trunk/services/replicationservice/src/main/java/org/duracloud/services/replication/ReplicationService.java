36b2fa0618e1baf59c92c7dbfa9d455298f8c830
Andrew Woods <awoods@duraspace.org>
2009-08-24 23:01:23 +0000

    This update adds OSGi integration testing to 'replicationservice'. It uses PAX-Exam without the need to hard-code the test dependencies into the test class(es). The update also adds dynamic configuration to the ReplicationService.
    git-svn-id: https://svn.duraspace.org/duracloud-priv@143 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- trunk/services/replicationservice/src/main/java/org/duracloud/services/replication/ReplicationService.java
+++ trunk/services/replicationservice/src/main/java/org/duracloud/services/replication/ReplicationService.java
@@ -1,4 +1,7 @@
+
 package org.duracloud.services.replication;
+
+import java.util.Dictionary;
 
 import javax.jms.Destination;
 import javax.jms.JMSException;
@@ -10,49 +13,46 @@ import javax.jms.TextMessage;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import org.duracloud.services.ComputeService;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.listener.AbstractMessageListenerContainer;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
 
 public class ReplicationService
-        implements ComputeService, MessageListener {
+        implements ComputeService, MessageListener, ManagedService {
 
     protected static final String STORE_ID = "storeId";
+
     protected static final String SPACE_ID = "spaceId";
+
     protected static final String CONTENT_ID = "contentId";
 
     private static final Logger log =
-        LoggerFactory.getLogger(ReplicationService.class);
+            LoggerFactory.getLogger(ReplicationService.class);
 
     private String host;
+
     private String port;
+
     private String context;
+
     private String brokerURL;
+
     private String fromStoreId;
+
     private String toStoreId;
+
     private String replicationType;
 
     private AbstractMessageListenerContainer jmsContainer;
-    private ActiveMQConnectionFactory connectionFactory;
-    private Destination destination;
-    private Replicator replicator;
 
-    public ReplicationService(String host,
-                              String port,
-                              String context,
-                              String brokerURL,
-                              String fromStoreId,
-                              String toStoreId,
-                              String replicationType) {
-        this.host = host;
-        this.port = port;
-        this.context = context;
-        this.brokerURL = brokerURL;
-        this.fromStoreId = fromStoreId;
-        this.toStoreId = toStoreId;
-        this.replicationType = replicationType;
-    }
+    private ActiveMQConnectionFactory connectionFactory;
+
+    private Destination destination;
+
+    private Replicator replicator;
 
     public void start() throws Exception {
         log.info("Starting Replication Service");
@@ -77,9 +77,11 @@ public class ReplicationService
         jmsContainer.start();
         jmsContainer.initialize();
 
-        replicator = new Replicator(host, port, context, fromStoreId, toStoreId);
+        replicator =
+                new Replicator(host, port, context, fromStoreId, toStoreId);
 
-        System.out.println("Listener container started: " + jmsContainer.isRunning());
+        System.out.print("Listener container started: ");
+        System.out.println("jmsContainer.isRunning()");
         System.out.println("**********");
         log.info("Replication Service Listener Started");
     }
@@ -110,29 +112,33 @@ public class ReplicationService
     }
 
     public void onMessage(Message message) {
-        if(log.isDebugEnabled()) {
+        if (log.isDebugEnabled()) {
             log.debug("Message recieved in Replication Service: " + message);
         }
         System.out.println("Message Received: " + message); //TODO: Remove once logging works
 
-        if(message instanceof MapMessage) {
-            handleMapMessage((MapMessage)message);
-        } else if(message instanceof TextMessage) {
-            handleTextMessage(((TextMessage)message));
+        if (message instanceof MapMessage) {
+            handleMapMessage((MapMessage) message);
+        } else if (message instanceof TextMessage) {
+            handleTextMessage(((TextMessage) message));
         } else {
-          String error = "Message received which cannot be processed: " + message;
-          log.warn(error);
-          System.out.println(error); //TODO: Remove once logging works
+            String error =
+                    "Message received which cannot be processed: " + message;
+            log.warn(error);
+            System.out.println(error); //TODO: Remove once logging works
         }
     }
 
     private void handleTextMessage(TextMessage message) {
         try {
             String msgText = message.getText();
-            log.warn("Text message received in replication service: " + msgText);
-            System.out.println("Text message received: " + msgText); //TODO: Remove once logging works
-        } catch(JMSException je) {
-            String error = "Error occured processing text message: " + je.getMessage();
+            String msg =
+                    "Text message received in replication service: " + msgText;
+            log.warn(msg);
+            System.out.println(msg); //TODO: Remove once logging works
+        } catch (JMSException je) {
+            String error =
+                    "Error occured processing text message: " + je.getMessage();
             log.error(error);
             throw new RuntimeException(error, je);
         }
@@ -143,11 +149,75 @@ public class ReplicationService
             String spaceId = message.getString(SPACE_ID);
             String contentId = message.getString(CONTENT_ID);
             replicator.replicateContent(spaceId, contentId);
-        } catch(JMSException je) {
-            String error = "Error occured processing map message: " + je.getMessage();
+        } catch (JMSException je) {
+            String error =
+                    "Error occured processing map message: " + je.getMessage();
             log.error(error);
             throw new RuntimeException(error, je);
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void updated(Dictionary properties) throws ConfigurationException {
+        // Implementation not needed. Update performed through setters.
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getContext() {
+        log.debug("getContext(): " + context);
+        return context;
+    }
+
+    public void setContext(String context) {
+        log.debug("setContext(): " + context);
+        this.context = context;
+    }
+
+    public String getBrokerURL() {
+        return brokerURL;
+    }
+
+    public void setBrokerURL(String brokerURL) {
+        this.brokerURL = brokerURL;
+    }
+
+    public String getFromStoreId() {
+        return fromStoreId;
+    }
+
+    public void setFromStoreId(String fromStoreId) {
+        this.fromStoreId = fromStoreId;
+    }
+
+    public String getToStoreId() {
+        return toStoreId;
+    }
+
+    public void setToStoreId(String toStoreId) {
+        this.toStoreId = toStoreId;
+    }
+
+    public String getReplicationType() {
+        return replicationType;
+    }
+
+    public void setReplicationType(String replicationType) {
+        this.replicationType = replicationType;
     }
 
 }
