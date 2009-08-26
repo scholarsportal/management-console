cac6aefd9d1b8924a142c3ec4ea3cb0aedbed56d
Andrew Woods <awoods@duraspace.org>
2009-08-26 21:20:40 +0000

    This update replaces the third-party emc-cloud-storage library with the latest version. It also makes a small modification to the EMCStorageProvider.java to get the unit tests working. Lastly, it brings the emcstorageprovider project back into the build.
    git-svn-id: https://svn.duraspace.org/duracloud-priv@146 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- trunk/resources/scripts/installLibs.bat
+++ trunk/resources/scripts/installLibs.bat
@@ -6,7 +6,7 @@ call mvn install:install-file -Dfile=../libs/aws-typica-1.5.1.jar -DgroupId=com.
 REM call mvn install:install-file -Dfile=../libs/amazon-ec2-2008-12-01-java-library-src.jar -DgroupId=com.amazonaws -DartifactId=ec2-src -Dversion=2008-12-01 -Dpackaging=jar
 call mvn install:install-file -Dfile=../libs/rackspace-java-cloudfiles-1.3.0.jar -DgroupId=com.mosso -DartifactId=cloudfiles -Dversion=1.3.0 -Dpackaging=jar
 call mvn install:install-file -Dfile=../libs/rackspace-java-cloudfiles-1.3.0-src.jar -DgroupId=com.mosso -DartifactId=cloudfiles-src -Dversion=1.3.0 -Dpackaging=jar
-call mvn install:install-file -Dfile=../libs/emcesu-0.1.jar -DgroupId=com.emc -DartifactId=emcesu -Dversion=0.1 -Dpackaging=jar
+call mvn install:install-file -Dfile=../libs/emcesu-1.2.0.90.jar -DgroupId=com.emc -DartifactId=emcesu -Dversion=1.2.0.90 -Dpackaging=jar
 call mvn install:install-file -Dfile=../libs/sun-object-client-0.2.jar -DgroupId=com.sun.cloud.api -DartifactId=object-client -Dversion=0.2 -Dpackaging=jar
 call mvn install:install-file -Dfile=../libs/sun-object-client-0.2-src.jar -DgroupId=com.sun.cloud.api -DartifactId=object-client-src -Dversion=0.2 -Dpackaging=jar
 call mvn install:install-file -Dfile=../libs/com.springsource.org.apache.derby-10.5.1000001.764942-duracloud.jar -DgroupId=org.apache.derby -DartifactId=com.springsource.org.apache.derby -Dversion=10.5.1000001.764942-duracloud -Dpackaging=jar
