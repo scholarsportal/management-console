4803e5d66d6f08467df1ed7ffec3b71833ead876
Daniel Bernstein <dbernstein@duraspace.org>
2009-09-14 16:55:57 +0000

    derby jar now references the mainwebapp instead of durastore.
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@165 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- resources/scripts/unitDBUtil.sh
+++ resources/scripts/unitDBUtil.sh
@@ -16,6 +16,7 @@ if [ -z "$BUILD_HOME" ]; then
 fi
 
 export CUSTOMER_LIB=${BUILD_HOME}/durastore/target/durastore-1.0.0/WEB-INF/lib
+export MAIN_LIB=${BUILD_HOME}/mainwebapp/target/mainwebapp-1.0.0/WEB-INF/lib
 
 # Set classpath to jars normally found in durastore war
 export CLASSPATH=$CLASSPATH:$CUSTOMER_LIB/aopalliance-1.0.jar
@@ -30,7 +31,7 @@ export CLASSPATH=$CLASSPATH:$CUSTOMER_LIB/commons-io-1.4.jar
 export CLASSPATH=$CLASSPATH:$CUSTOMER_LIB/commons-lang-2.4.jar
 export CLASSPATH=$CLASSPATH:$CUSTOMER_LIB/commons-logging-1.0.jar
 export CLASSPATH=$CLASSPATH:$CUSTOMER_LIB/commons-pool-1.3.jar
-export CLASSPATH=$CLASSPATH:$CUSTOMER_LIB/com.springsource.org.apache.derby-10.5.1000001.764942.jar
+export CLASSPATH=$CLASSPATH:$MAIN_LIB/com.springsource.org.apache.derby-10.5.1000001.764942-duracloud.jar
 export CLASSPATH=$CLASSPATH:$CUSTOMER_LIB/javassist-3.6.ga.jar
 export CLASSPATH=$CLASSPATH:$CUSTOMER_LIB/jaxb-api-2.1.jar
 export CLASSPATH=$CLASSPATH:$CUSTOMER_LIB/jaxb-impl-2.1.jar
