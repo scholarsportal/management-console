354f55c60131c03b0e32bd726a49ded3011974d8
Bill Branan <bbranan@duraspace.org>
2009-09-15 19:37:00 +0000

    Adds a script to build and test DuraService and ServiceClient. The reason for the break from the traditional build/test setup is that in order to run the integration tests for these projects ServicesAdmin must be deployed and running inside of an OSGi container, something that is difficult to achieve in maven directly. Also, for some reason the run-deps.sh script does not run properly on the bamboo server (it skips the dependencies for common) so the pom-run.xml has been included in this commit for ServicesAdmin only. We should determine why the pax script fails to perform correctly on the bamboo server and remove pom-run.xml, but until then, if you update the pom.xml for servicesadmin, please also update pom-run.xml.
    git-svn-id: https://svn.duraspace.org/duracloud-priv@166 3c0c4179-e91d-42e7-a082-8e2792ce3676

---
+++ trunk/resources/scripts/services.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+echo "========================="
+echo "Starting services tests...."
+echo "========================="
+echo ""
+
+export PATH=$PATH:/opt/pax/pax-construct-1.4/bin
+
+echo "=================="
+echo "Starting Services Admin..."
+echo "=================="
+echo ""
+
+cd $BUILD_HOME/services/servicesadmin
+$MVN clean install -f pom-run.xml -Dmaven.test.skip=true pax:provision > provision.log &
+sleep 30
+
+echo ""
+echo "==================================="
+echo "Compiling & running unit & integration tests for DuraService..."
+echo "==================================="
+cd $BUILD_HOME/duraservice
+$MVN clean install -Dtomcat.port.default=9090 -Dlog.level.default=DEBUG
+
+echo ""
+echo "==================================="
+echo "Compiling & running unit & integration tests for Service Client..."
+echo "==================================="
+cd $BUILD_HOME/serviceclient
+$MVN clean install -Dtomcat.port.default=9090 -Dlog.level.default=DEBUG
+
+echo "======================="
+echo "Shutting Down Services Admin..."
+echo "======================="
+echo ""
+kill %1
+
+echo ""
+echo "===================================="
+echo "Completed services tests successfully!"
+echo "===================================="
+cd $BUILD_HOME
