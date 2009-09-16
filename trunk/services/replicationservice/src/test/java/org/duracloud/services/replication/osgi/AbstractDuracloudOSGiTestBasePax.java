7ba08cf8187c076a9b75915ae149f52d4d23c4a7
Andrew Woods <awoods@duraspace.org>
2009-09-16 20:07:19 +0000

    This update adds cross-OSGi-container testing to servicesutil, serviceadmin, and replicationservice.
    git-svn-id: https://svn.duraspace.org/duracloud-priv@169 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- trunk/services/replicationservice/src/test/java/org/duracloud/services/replication/osgi/AbstractDuracloudOSGiTestBasePax.java
+++ trunk/services/replicationservice/src/test/java/org/duracloud/services/replication/osgi/AbstractDuracloudOSGiTestBasePax.java
@@ -4,6 +4,7 @@ package org.duracloud.services.replication.osgi;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 
+import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Inject;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
@@ -11,7 +12,6 @@ import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.osgi.framework.BundleContext;
 
 import static org.ops4j.pax.exam.CoreOptions.bundle;
-import static org.ops4j.pax.exam.CoreOptions.equinox;
 import static org.ops4j.pax.exam.CoreOptions.mavenConfiguration;
 import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.CoreOptions.provision;
@@ -34,9 +34,14 @@ public class AbstractDuracloudOSGiTestBasePax {
         Option bundles =
                 provision(bundle("file:target/replicationservice-1.0.0.jar"));
 
+        Option frameworks = CoreOptions.frameworks(CoreOptions.equinox(),
+        // Knopflerfish does not like the felix.configadmin bundle
+                                                   //  CoreOptions.knopflerfish(),
+                                                   CoreOptions.felix());
+
         return options(bundles,
                        mavenConfiguration(),
-                       equinox(),
+                       frameworks,
                        profile("spring.dm"),
                        profile("log"));
     }
