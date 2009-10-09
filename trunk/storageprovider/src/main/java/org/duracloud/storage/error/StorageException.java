1b0cbb38e6c3779b0a8d281941b050cf0bbd5bf3
Andrew Woods <awoods@duraspace.org>
2009-10-09 21:29:26 +0000

    This update isolates exceptions in EMCStorageProvider.
    git-svn-id: https://svn.duraspace.org/duracloud-priv@218 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- trunk/storageprovider/src/main/java/org/duracloud/storage/error/StorageException.java
+++ trunk/storageprovider/src/main/java/org/duracloud/storage/error/StorageException.java
@@ -15,7 +15,7 @@ public class StorageException extends RuntimeException {
     protected boolean retry;
 
     public StorageException(String message) {
-        this(message, false);
+        this(message, NO_RETRY);
     }
 
     public StorageException(String message, boolean retry) {
@@ -24,17 +24,21 @@ public class StorageException extends RuntimeException {
     }
 
     public StorageException(String message, Throwable throwable) {
-        this(message, throwable, false);
+        this(message, throwable, NO_RETRY);
     }
 
-    public StorageException(String message, Throwable throwable,
-                            boolean retry) {
+    public StorageException(String message, Throwable throwable, boolean retry) {
         super(message, throwable);
         this.retry = retry;
     }
 
     public StorageException(Throwable throwable) {
+        this(throwable, NO_RETRY);
+    }
+
+    public StorageException(Throwable throwable, boolean retry) {
         super(throwable);
+        this.retry = retry;
     }
 
     public boolean isRetry() {
