03df046e3ba45e84e336e9ff325404125e1229b5
Andrew Woods <awoods@duraspace.org>
2009-11-08 20:04:08 +0000

    This update adds 'serviceconfig' project to top-level pom.xml. It also includes minor updates to the services-config.xsd.
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@312 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- serviceconfig/src/main/java/org/duracloud/serviceconfig/ServicesConfigDocument.java
+++ serviceconfig/src/main/java/org/duracloud/serviceconfig/ServicesConfigDocument.java
@@ -11,6 +11,7 @@ import java.util.List;
  *         Date: Nov 6, 2009
  */
 public interface ServicesConfigDocument {
+
     public String getVersion();
 
     public List<ServiceInfo> getServiceInfos();
