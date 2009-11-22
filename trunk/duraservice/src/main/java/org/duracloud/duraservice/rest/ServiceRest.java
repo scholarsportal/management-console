ccc0501f8c70b547d716c59f2456f7319b59e4b3
Bill Branan <bbranan@duraspace.org>
2009-11-22 05:00:33 +0000

    Updates serviceconfig to return an empty services tag when there are no services in the list rather than an xml fragment tag. A couple other minor tweaks as well.
    git-svn-id: https://svn.duraspace.org/duracloud-priv@366 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- trunk/duraservice/src/main/java/org/duracloud/duraservice/rest/ServiceRest.java
+++ trunk/duraservice/src/main/java/org/duracloud/duraservice/rest/ServiceRest.java
@@ -13,6 +13,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
@@ -102,6 +103,7 @@ public class ServiceRest extends BaseRest {
      */
     @Path("/services")
     @GET
+    @Produces(XML)
     public Response getServices(@QueryParam("show")
                                 String show) {
         ResponseBuilder response = Response.ok();
@@ -139,6 +141,7 @@ public class ServiceRest extends BaseRest {
      */
     @Path("/services/{serviceId}")
     @GET
+    @Produces(XML)
     public Response getService(@PathParam("serviceId")
                                int serviceId) {
         ResponseBuilder response = Response.ok();
@@ -170,6 +173,7 @@ public class ServiceRest extends BaseRest {
      */
     @Path("/services/{serviceId}/{deploymentId}")
     @GET
+    @Produces(XML)
     public Response getDeployedService(@PathParam("serviceId")
                                        int serviceId,
                                        @PathParam("deploymentId")
