58eaf3ee100a68ec0ff6165909496e4e2b4e9c45
Andrew Woods <awoods@duraspace.org>
2010-04-20 12:47:37 +0000

    This update adds the ability to initialize duradmin security users via a rest-like request.
    git-svn-id: https://svn.duraspace.org/duracloud-priv@580 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- trunk/duradmin/src/test/java/org/duracloud/duradmin/TestDuradmin.java
+++ trunk/duradmin/src/test/java/org/duracloud/duradmin/TestDuradmin.java
@@ -1,16 +1,9 @@
 
 package org.duracloud.duradmin;
 
-import junit.framework.TestCase;
-import org.duracloud.common.model.Credential;
-import org.duracloud.common.model.DuraCloudUserType;
 import org.duracloud.common.web.RestHttpHelper;
 import org.duracloud.common.web.RestHttpHelper.HttpResponse;
-import org.duracloud.duradmin.config.DuradminConfig;
-import org.duracloud.unittestdb.UnitTestDatabaseUtil;
-import org.duracloud.unittestdb.domain.ResourceType;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -19,63 +12,18 @@ import org.junit.Test;
  * 
  * @author Bill Branan
  */
-public class TestDuradmin
-        extends TestCase {
+public class TestDuradmin extends DuradminTestBase {
 
-    private static RestHttpHelper restHelper = getAuthorizedRestHelper();
-
-    private static String baseUrl;
-
-    private static String configFileName = "test-duradmin.properties";
-    static {
-        DuradminConfig.setConfigFileName(configFileName);
-    }
-
-    @Override
-    @Before
-    protected void setUp() throws Exception {
-        String host = DuradminConfig.getPropsHost();
-        String port = DuradminConfig.getPropsPort();
-        baseUrl = "http://" + host + ":" + port + "/duradmin";
-    }
-
-    @Override
-    @After
-    protected void tearDown() throws Exception {
-    }
+    private static RestHttpHelper restHelper = RestTestHelper.getAuthorizedRestHelper();
 
     @Test
     public void testSpaces() throws Exception {
-        String url = baseUrl + "/spaces.htm";
+        String url = getBaseUrl() + "/spaces.htm";
         HttpResponse response = restHelper.get(url);
-        assertEquals(200, response.getStatusCode());
+        Assert.assertEquals(200, response.getStatusCode());
 
         String responseText = response.getResponseBody();
-        assertNotNull(responseText);
-    }
-
-    private static RestHttpHelper getAuthorizedRestHelper() {
-        return new RestHttpHelper(getRootCredential());
-    }
-
-    private static Credential getRootCredential() {
-        UnitTestDatabaseUtil dbUtil = null;
-        try {
-            dbUtil = new UnitTestDatabaseUtil();
-        } catch (Exception e) {
-            System.err.println("ERROR from unitTestDB: " + e.getMessage());
-        }
-
-        Credential rootCredential = null;
-        try {
-            ResourceType rootUser = ResourceType.fromDuraCloudUserType(
-                DuraCloudUserType.ROOT);
-            rootCredential = dbUtil.findCredentialForResource(rootUser);
-        } catch (Exception e) {
-            System.err.print("ERROR getting credential: " + e.getMessage());
-
-        }
-        return rootCredential;
+        Assert.assertNotNull(responseText);
     }
 
 }
