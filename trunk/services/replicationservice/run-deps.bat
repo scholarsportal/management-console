62142f4768b9d7c5ec7ad539f371d1539b4ba1c6
Bill Branan <bbranan@duraspace.org>
2010-05-27 17:36:39 +0000

    Updates run-deps for several services to allow bundles to start properly
    git-svn-id: https://svn.duraspace.org/duracloud-priv@699 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- trunk/services/replicationservice/run-deps.bat
+++ trunk/services/replicationservice/run-deps.bat
@@ -4,9 +4,9 @@ call ../../resources/scripts/osgi/env.bat
 copy pom.xml pom.xml.bak
 
 REM - Spring DM
-call pax-import-bundle -g org.springframework.osgi -a spring-osgi-core -v 1.2.0
-call pax-import-bundle -g org.springframework.osgi -a spring-osgi-extender -v 1.2.0
-call pax-import-bundle -g org.springframework.osgi -a spring-osgi-io -v 1.2.0
+REM call pax-import-bundle -g org.springframework.osgi -a spring-osgi-core -v 1.2.0
+REM call pax-import-bundle -g org.springframework.osgi -a spring-osgi-extender -v 1.2.0
+REM call pax-import-bundle -g org.springframework.osgi -a spring-osgi-io -v 1.2.0
 
 REM - DuraCloud
 call pax-import-bundle -g org.duracloud -a storageprovider -v %PROJECT_VERSION% -- -DimportTransitive -DwidenScope
@@ -18,6 +18,7 @@ call pax-import-bundle -g javax.activation -a com.springsource.javax.activation 
 call pax-import-bundle -g org.slf4j -a com.springsource.slf4j.log4j -v 1.5.0 -- -DimportTransitive -DwidenScope
 call pax-import-bundle -g com.thoughtworks.xstream -a com.springsource.com.thoughtworks.xstream -v 1.3.0 -- -DimportTransitive -DwidenScope
 call pax-import-bundle -g org.jdom -a com.springsource.org.jdom -v 1.0.0
+call pax-import-bundle -g javax.servlet -a com.springsource.javax.servlet -v 2.5.0
 
 copy pom.xml pom-run.xml
 move pom.xml.bak pom.xml
--- trunk/services/replicationservice/run-deps.sh
+++ trunk/services/replicationservice/run-deps.sh
@@ -9,9 +9,9 @@ scriptdir=`dirname "$0"`
 
 
 # - Spring DM -
-pax-import-bundle -g org.springframework.osgi -a spring-osgi-core -v 1.2.0
-pax-import-bundle -g org.springframework.osgi -a spring-osgi-extender -v 1.2.0
-pax-import-bundle -g org.springframework.osgi -a spring-osgi-io -v 1.2.0
+# pax-import-bundle -g org.springframework.osgi -a spring-osgi-core -v 1.2.0
+# pax-import-bundle -g org.springframework.osgi -a spring-osgi-extender -v 1.2.0
+# pax-import-bundle -g org.springframework.osgi -a spring-osgi-io -v 1.2.0
 
 # - DuraCloud -
 pax-import-bundle -g org.duracloud -a storageprovider -v ${PROJECT_VERSION} -- -DimportTransitive -DwidenScope
@@ -23,6 +23,7 @@ pax-import-bundle -g javax.activation -a com.springsource.javax.activation -v 1.
 pax-import-bundle -g org.slf4j -a com.springsource.slf4j.log4j -v 1.5.0 -- -DimportTransitive -DwidenScope
 pax-import-bundle -g com.thoughtworks.xstream -a com.springsource.com.thoughtworks.xstream -v 1.3.0 -- -DimportTransitive -DwidenScope
 pax-import-bundle -g org.jdom -a com.springsource.org.jdom -v 1.0.0
+pax-import-bundle -g javax.servlet -a com.springsource.javax.servlet -v 2.5.0
 
 mvn clean pax:provision -Dmaven.test.skip=true
 chmod +x runner/run.sh
