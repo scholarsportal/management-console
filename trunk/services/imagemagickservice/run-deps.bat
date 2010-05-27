38f14704a11e3d69eed87e925248f3947ee993ba
Bill Branan <bbranan@duraspace.org>
2010-05-27 16:06:29 +0000

    Resolves DURACLOUD-155 by adding an env.bat file and allowing all of the services batch files to depend on it
    git-svn-id: https://svn.duraspace.org/duracloud-priv@698 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- trunk/services/imagemagickservice/run-deps.bat
+++ trunk/services/imagemagickservice/run-deps.bat
@@ -1,14 +1,17 @@
+REM - Load env
+call ../../resources/scripts/osgi/env.bat
+
 copy pom.xml pom.xml.bak
 
-# - DuraCloud -
-call pax-import-bundle -g org.duracloud -a common -v 0.4.0-SNAPSHOT -- -DimportTransitive -DwidenScope
-call pax-import-bundle -g org.duracloud.services -a scriptservice -v 0.4.0-SNAPSHOT -- -DimportTransitive -DwidenScope
+REM - DuraCloud
+call pax-import-bundle -g org.duracloud -a common -v %PROJECT_VERSION% -- -DimportTransitive -DwidenScope
+call pax-import-bundle -g org.duracloud.services -a scriptservice -v %PROJECT_VERSION% -- -DimportTransitive -DwidenScope
 
-# - Other -
+REM - Other
 call pax-import-bundle -g org.slf4j -a com.springsource.slf4j.log4j -v 1.5.0 -- -DimportTransitive -DwidenScope
 call pax-import-bundle -g com.thoughtworks.xstream -a com.springsource.com.thoughtworks.xstream -v 1.3.0 -- -DimportTransitive -DwidenScope
 
-call mvn clean pax:provision -Dmaven.test.skip=true
-
 copy pom.xml pom-run.xml
 move pom.xml.bak pom.xml
+
+call mvn clean -f pom-run.xml pax:provision -Dmaven.test.skip=true
