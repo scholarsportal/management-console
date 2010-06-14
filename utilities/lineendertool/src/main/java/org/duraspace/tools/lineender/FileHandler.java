104af162e03d4150b4a1df99b9bfb4ce999634a7
Bill Branan <bbranan@duraspace.org>
2010-06-14 02:27:05 +0000

    Adds a tool for updating the end-of-line characters in Java files
    git-svn-id: https://svn.duraspace.org/duracloud-priv@745 3c0c4179-e91d-42e7-a082-8e2792ce3676

---
+++ utilities/lineendertool/src/main/java/org/duraspace/tools/lineender/FileHandler.java
@@ -0,0 +1,16 @@
+package org.duraspace.tools.lineender;
+
+import java.io.File;
+
+/*
+ * @author: Bill Branan
+ * Date: June 11, 2010
+ */
+public interface FileHandler {
+
+    /**
+     * Allows the handler to deal with a file
+     */
+    public void handleFile(File file);
+
+}
