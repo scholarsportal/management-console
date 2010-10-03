4b2ca096140835f1da7003f0d920d9a6a8b41859
Daniel Bernstein <dbernstein@duraspace.org>
2010-10-03 23:06:55 +0000

    added getters and setters to duracloud user class
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@847 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- account-management-util/src/main/java/org/duracloud/account/util/DuracloudUser.java
+++ account-management-util/src/main/java/org/duracloud/account/util/DuracloudUser.java
@@ -1,12 +1,40 @@
 package org.duracloud.account.util;
 
 import org.duracloud.security.domain.SecurityUserBean;
-
+/**
+ * 
+ * @author "Daniel Bernstein (dbernstein@duraspace.org)"
+ *
+ */
 public class DuracloudUser extends SecurityUserBean{
 	private String fullName;
 	private String email;
-	
-	public DuracloudUser(SecurityUserBean securityUserBean){
-		
+	/**
+	 * 
+	 * @return
+	 */
+	public String getFullName() {
+		return fullName;
+	}
+	/**
+	 * 
+	 * @param fullName
+	 */
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * 
+	 * @param email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
