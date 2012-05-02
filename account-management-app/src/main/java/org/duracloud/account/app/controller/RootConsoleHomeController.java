1539839279bfab3994551bbeb7460f75a834828e
Bill Branan <bbranan@duraspace.org>
2012-05-02 20:45:31 +0000

    This update resolves issue #6 specified in https://wiki.duraspace.org/display/DSPINT/MC+Release+1.0+Tasks: Removes the home tab on the root console. Work done by Danny Bernstein.
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@1270 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- account-management-app/src/main/java/org/duracloud/account/app/controller/RootConsoleHomeController.java
+++ account-management-app/src/main/java/org/duracloud/account/app/controller/RootConsoleHomeController.java
@@ -3,6 +3,7 @@ package org.duracloud.account.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping(RootConsoleHomeController.BASE_MAPPING)
@@ -13,7 +14,8 @@ public class RootConsoleHomeController {
 
     @RequestMapping(value={"/", ""})
     public ModelAndView getHome(){
-        return new ModelAndView(ROOT_HOME_VIEW_ID);
+        return new ModelAndView(
+            new RedirectView(AccountsController.BASE_MAPPING, true));
     }
 
 }
--- account-management-app/src/main/java/org/duracloud/account/app/view/RootPreparer.java
+++ account-management-app/src/main/java/org/duracloud/account/app/view/RootPreparer.java
@@ -27,7 +27,6 @@ public class RootPreparer implements ViewPreparer {
 
         public RootTabs() {
             super();
-            add(new Tab(RootConsoleHomeController.BASE_MAPPING, "home"));
             add(new Tab("/root/accounts", "accounts"));
             add(new Tab("/root/clusters", "clusters"));
             add(new Tab("/root/users", "users"));
