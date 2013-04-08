464bdbb5719ccda682569536c1d567d4c0b4d9e1
Bill Branan <bbranan@duraspace.org>
2013-04-08 19:40:42 +0000

    Completes https://jira.duraspace.org/browse/DURACLOUDPRIV-228, adds Glacier storage provider option.
    git-svn-id: https://svn.duraspace.org/duracloud-priv/trunk@1382 3c0c4179-e91d-42e7-a082-8e2792ce3676

--- account-management-app/src/main/java/org/duracloud/account/util/StorageProviderTypeUtil.java
+++ account-management-app/src/main/java/org/duracloud/account/util/StorageProviderTypeUtil.java
@@ -1,21 +1,22 @@
 package org.duracloud.account.util;
 
+import org.duracloud.storage.domain.StorageProviderType;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import org.duracloud.storage.domain.StorageProviderType;
 
 public class StorageProviderTypeUtil {
     private static final List<StorageProviderType> SECONDARY_PROVIDER_TYPES;
     
     static {
-        SECONDARY_PROVIDER_TYPES = new ArrayList<StorageProviderType>(3);
+        SECONDARY_PROVIDER_TYPES = new ArrayList<>();
         SECONDARY_PROVIDER_TYPES.add(StorageProviderType.RACKSPACE);
         SECONDARY_PROVIDER_TYPES.add(StorageProviderType.MICROSOFT_AZURE);
         SECONDARY_PROVIDER_TYPES.add(StorageProviderType.SDSC);
+        SECONDARY_PROVIDER_TYPES.add(StorageProviderType.AMAZON_GLACIER);
     }
     
     public static List<StorageProviderType> getAvailableSecondaryTypes() {
-        return new ArrayList<StorageProviderType>(SECONDARY_PROVIDER_TYPES);
+        return new ArrayList<>(SECONDARY_PROVIDER_TYPES);
     }
 }
