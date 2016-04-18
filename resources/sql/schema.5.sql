17a68c67ddd22cbdf2aa5c11ef26b13ad00a5efd
Bill Branan <bbranan@duraspace.org>
2016-04-18 14:46:20 -0400

    Updates schema.5.sql to move the alter table calls up into the global_properties table creation call.  Also removes an unnecessary call to alter the server_details table because the table is dropped at the end anyway.

--- resources/sql/schema.5.sql
+++ resources/sql/schema.5.sql
@@ -2,18 +2,15 @@ CREATE TABLE IF NOT EXISTS `global_properties` (
  `id` bigint(20) NOT NULL AUTO_INCREMENT,
  `modified` datetime DEFAULT NULL,
  `instance_notification_topic_arn` varchar(255) NOT NULL,
+ `duracloud_root_username` varchar(255) NOT NULL DEFAULT 'root',
+ `duracloud_root_password` varchar(255) NOT NULL DEFAULT 'rpw',
+ `cloud_front_account_id` varchar(255) NOT NULL DEFAULT 'cloud front account id',
+ `cloud_front_key_id` varchar(255) NOT NULL DEFAULT 'cloud front key id',
+ `cloud_front_key_path` varchar(255) NOT NULL DEFAULT 'cloud front key path',
  PRIMARY KEY (`id`)
 ) CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS=0;
-
-alter table global_properties add column duracloud_root_username varchar(255) NOT NULL DEFAULT 'root';
-alter table global_properties add column duracloud_root_password varchar(255) NOT NULL DEFAULT 'rpw';
-alter table global_properties add column cloud_front_account_id varchar(255) NOT NULL DEFAULT 'cloud front account id';
-alter table global_properties add column cloud_front_key_id varchar(255) NOT NULL DEFAULT 'cloud front key id';
-alter table global_properties add column cloud_front_key_path varchar(255) NOT NULL DEFAULT 'cloud front key path';
-
-alter table server_details drop column compute_provider_account_id;
 
 drop table if exists server_image;
 drop table if exists duracloud_instance;
@@ -21,7 +18,7 @@ drop table if exists compute_provider_account;
 
 alter table account_info add column primary_storage_provider_account_id bigint(20) not null;
 
- update account_info, server_details  set account_info.primary_storage_provider_account_id  = server_details.primary_storage_provider_account_id where account_info.server_details_id = server_details.id;
+update account_info, server_details set account_info.primary_storage_provider_account_id = server_details.primary_storage_provider_account_id where account_info.server_details_id = server_details.id;
 
 alter table storage_provider_account add column account_info_id bigint(20) default null;
 alter table `storage_provider_account` add constraint foreign key `FK_STORAGE_PROVIDER_ACCOUNT_INFO` (`account_info_id`) references `account_info` (`id`);
@@ -32,11 +29,10 @@ alter table storage_provider_account drop foreign key `<server_details_foreign_k
 drop index `<server_details_foreign_key_name>` on storage_provider_account;
 alter table storage_provider_account drop column server_details_id;
 
-
 alter table account_info drop foreign key `<server_details_foreign_key>`;
 drop index `<server_details_foreign_key>` on account_info;
 alter table account_info drop column server_details_id;
 
 drop table server_details;
 
- SET FOREIGN_KEY_CHECKS=1;
+SET FOREIGN_KEY_CHECKS=1;
