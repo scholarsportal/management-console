09e15fe973e8e6bf397a9a671b2c9ebb5b52e4e4
Andy Foster <andy.foster@utoronto.ca>
2020-03-02 13:27:13 -0500

    Change SQL query to MODIFY
    Prevents repetition of column name.

--- resources/sql/update-non-aws.sql
+++ resources/sql/update-non-aws.sql
@@ -8,7 +8,7 @@ ADD COLUMN `rabbitmq_username` varchar(255),
 ADD COLUMN `rabbitmq_password` varchar(255);
 
 ALTER TABLE `global_properties`
-CHANGE `instance_notification_topic_arn` `instance_notification_topic_arn` VARCHAR(255) NULL,
+MODIFY `instance_notification_topic_arn` VARCHAR(255) NULL,
 ADD COLUMN `notifier_type` varchar(255) DEFAULT 'AWS' NOT NULL,
 ADD COLUMN `rabbitmq_host` varchar(255),
 ADD COLUMN `rabbitmq_port` int(11),
